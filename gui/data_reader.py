7bb00ea9527c0a59131aecfa0911735e2574ef9b
Zijun Chen <c.zijunchen@gmail.com>
2019-09-27 01:28:41 +1000

    Add InputDataReader for gui.

--- gui/data_reader.py
+++ gui/data_reader.py
@@ -34,10 +34,30 @@ class OutputDataReader():
         return [(self._read_float(), self._read_float()) for _ in range(self.n)]
 
 
+class InputDataReader():
+    def __init__(self, input_file: str) -> None:
+        with open(input_file, "r") as f:
+            self.n = int(f.readline())
+            self._pos = []
+            self._w = []
+            for _ in range(self.n):
+                (x, y, _, _, w) = map(float, f.readline().split())
+                self._pos.append((x, y))
+                self._w.append(w)
 
+    def get_init_pos(self) -> List[Tuple[float, float]]:
+        return self._pos
+
+    def get_weights(self) -> List[float]:
+        return self._w
 
 # reader = OutputDataReader("./../tmp.out")
 # print(reader.n)
 # print(reader.m)
 # print(reader.get_one_step())
+
+# reader = InputDataReader("./../tmp.in")
+# print(reader.n)
+# print(reader.get_init_pos())
+# print(reader.get_weights())
 
