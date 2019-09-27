b3954f9916fe1018ffdd46203d8b24633498bf03
PwzXxm <cepheidfov@gmail.com>
2019-09-28 01:27:28 +1000

    add algorithm switch support in main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils.h"
+#include "seq_quad_tree.h"
 
 void print_usage();
 
+static const char SEQ_QUAD_TREE[] = "seq_quad_tree";
+
 int main(int argc, char* argv[]) {
     if (argc != 6) {
+        printf("Wrong arguments\n");
         print_usage();
         return 1;
     }
@@ -15,7 +20,8 @@ int main(int argc, char* argv[]) {
     float t = atof(argv[3]);
     bool full_output = atoi(argv[4]);
     char *output_file = argv[5];
-    if (m <= 0 && t <= 0) {
+    if (m <= 0 || t <= 0) {
+        printf("Invalid step or time interval\n");
         print_usage();
         return 1;
     }
@@ -39,6 +45,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    if (strcmp(algo_name, SEQ_QUAD_TREE) == 0) {
+        algorithm_fun_ptr = &qt_sim;
+    } else {
+        printf("Unsupported algorithm\n");
+        print_usage();
+        return 1;
+    }
+
+    (*algorithm_fun_ptr)(m, n, t, parts, fp, full_output);
+
     output_particle_pos(n, parts, fp);
 
 
@@ -50,7 +66,8 @@ int main(int argc, char* argv[]) {
 
 
 void print_usage() {
-    printf("Wrong arguments\n");
     printf("Usage: nbody <algorithm name> <num of steps> <time of each step> <full output> <output file>\n");
     printf("Example: nbody seq_naive 100 0.01\n");
+    printf("Supported algorithms:\n");
+    printf("\tseq_quad_tree\n");
 }
