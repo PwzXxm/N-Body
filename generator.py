52fc906320da27d43d7dccb55cd85f81e4fa79d1
Zijun Chen <c.zijunchen@gmail.com>
2019-09-28 15:04:11 +1000

    [Generator] Change data range.

--- generator.py
+++ generator.py
@@ -5,10 +5,12 @@ def generate_data(n: int) -> None:
     for _ in range(n):
         px = random.uniform(-100, 100)
         py = random.uniform(-100, 100)
-        vx = random.uniform(-10, 10)
-        vy = random.uniform(-10, 10)
-        w = random.uniform(0.01, 2)
+        vx = random.uniform(-5, 5)
+        vy = random.uniform(-5, 5)
+        # vx = 0
+        # vy = 0
+        w = random.uniform(0.01, 20)
         print(px, py, vx, vy, w)
 
 
-generate_data(30)
+generate_data(100)
